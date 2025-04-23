d1afead839d31c16c0b9a7b00b6d4a91792cd2f8
Arshdeep <harkiratkaur8c6@gmail.com>
2025-04-23 11:24:30 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const mysql = require("mysql2");
+const app = express();
+const path = require("path");
+
+app.use(express.urlencoded(({extended: true})));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/patient", (req, res) => {
+   res.render("patient.ejs");
+});
+app.get("/careConnect", (req, res) => {
+  res.render("careConnect.ejs");
+});
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+app.post("/dashboard", (req, res) => {
+  let {Email: email, Username: username, Password: password, Category} = req.body;
+  if(Category == "Patient"){
+    let q = `SELECT count(username) FROM Patient`;
+    connection.query(q, (err, result) => {
+      if(err) throw err;
+      let id = result[0]["count(username)"];
+      let q = `INSERT INTO Patient (id, email, username, password) VALUES (${id}, "${email}", "${username}", "${password}")`;
+    try{
+      connection.query(q, (err, result) => {
+        if(err) throw err;
+        res.render("patient.ejs");
+      })
+    }catch(err){
+        console.log(`Error Found: ${err}`);
+    }
+    });
+  }else{
+    let q = `SELECT count(username) FROM Doctor`;
+    connection.query(q, (err, result) => {
+      if(err) throw err;
+      let id = result[0]["count(username)"];
+      let q = `INSERT INTO Doctor (id, email, username, password) VALUES (${id}, "${email}", "${username}", "${password}")`;
+    try{
+      connection.query(q, (err, result) => {
+        if(err) throw err;
+        res.render("doctor.ejs");
+      })
+    }catch(err){
+        console.log(`Error Found: ${err}`);
+    }
+    });
+  }
+});
+app.post("/consultation", (req, res) => {
+  let q = "SELECT username FROM Doctor";
+  try{
+    connection.query(q, (err, result) =>{
+      if(err) throw err;
+      let doctors = [];
+      for(let i = 0; i<result.length; i++){
+         doctors.push(result[i]["username"]);
+      }
+      res.render("consultation.ejs", {doctors});
+    });
+  }catch(err){
+    console.log(`Error Occured: ${err}`);
+  }
+});
+app.listen(8080, () => {
+    console.log(`Listening Started At 8080`);
+});
+// SETTING UP THE CONNECTION BETWEEN DATABASE AND INDEX.JS
+const connection = mysql.createConnection({
+  host: 'localhost', 
+  user:'root', 
+  database: 'CareConnect', 
+  password: 'Arshbanwait2@'
+});
